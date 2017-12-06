b28d237a57caa16082f3251296d07a80f3f54367
stone <stone9691@163.com>
2017-12-07 00:02:31 +0800

    Add buttom tab
    Signed-off-by: stone <stone9691@163.com>

--- app/src/main/java/com/example/stone/uidemo/MainActivity.java
+++ app/src/main/java/com/example/stone/uidemo/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.stone.uidemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import org.greenrobot.eventbus.EventBus;
+import me.majiajie.pagerbottomtabstrip.NavigationController;
+import me.majiajie.pagerbottomtabstrip.PageNavigationView;
+import me.majiajie.pagerbottomtabstrip.listener.OnTabItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,17 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PageNavigationView tab = (PageNavigationView) findViewById(R.id.main_activity_bottom_tab);
+
+        NavigationController navigationController = tab.material()
+                .addItem(android.R.drawable.ic_menu_camera, "Texts")
+                .addItem(android.R.drawable.ic_menu_compass, "Decks")
+                .addItem(android.R.drawable.ic_menu_search, "Dictionary")
+                .addItem(android.R.drawable.ic_menu_help, "Store")
+                .addItem(android.R.drawable.ic_menu_more, "More")
+                .build();
+
+        navigationController.addTabItemSelectedListener(new OnTabItemSelectedListener() {
+            @Override
+            public void onSelected(int index, int old) {
+                //选中时触发
+            }
+
+            @Override
+            public void onRepeat(int index) {
+                //重复选中时触发
+            }
+        });
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        EventBus.getDefault().register(this);
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        EventBus.getDefault().unregister(this);
     }
 }
