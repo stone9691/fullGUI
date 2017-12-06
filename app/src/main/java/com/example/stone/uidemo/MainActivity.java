791423b46dc1573fd3c7549c5dc0c93cf94cd61a
stone <stone9691@163.com>
2017-12-06 23:11:59 +0800

    initial commit
    Signed-off-by: stone <stone9691@163.com>

---
+++ app/src/main/java/com/example/stone/uidemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.stone.uidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+}
