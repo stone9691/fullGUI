ecd3837409365ae2d7e73565ea8b6d25f4f4a32e
stone <stone9691@163.com>
2017-12-14 23:25:18 +0800

    Implement text view activity
    Signed-off-by: stone <stone9691@163.com>

--- app/src/main/java/com/gammakite/fullchinese/view/text/TextViewActivity.java
+++ app/src/main/java/com/gammakite/fullchinese/view/text/TextViewActivity.java
@@ -1,7 +1,10 @@
 package com.gammakite.fullchinese.view.text;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.gammakite.fullchinese.R;
 
@@ -11,5 +14,13 @@ public class TextViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_view);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            LayoutInflater mInflater = LayoutInflater.from(this);
+            View mCustomView = mInflater.inflate(R.layout.text_view_action_bar, null);
+            actionBar.setCustomView(mCustomView);
+            actionBar.setDisplayShowCustomEnabled(true);
+        }
     }
 }
